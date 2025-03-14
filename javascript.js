ef6b0dd14fc921eb1a881764e7e1f5c55c7c432d
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-03-15 01:39:57 +0600

    added computer choice function.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
 console.log("hello");
+
+function getComputerChoice(){
+
+     let randomNumber= Math.floor(Math.random() * 3); // expected result 0 or 1 or 2
+     let comChoice= 0;
+
+     if(randomNumber === 0){
+         comChoice= "rock";
+
+     }
+     else if (randomNumber===1){
+         comChoice= "paper";
+
+     }
+     else {
+        comChoice="scissors";
+     }
+
+    return comChoice;
+
+}
+
+let comChoice= getComputerChoice();
+console.log(comChoice);
+
+
+
